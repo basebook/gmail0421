621862d7adc62d383956984efe17d251fca452de
wuxiang <wuxiangoutlook.com.cn>
2021-05-06 18:02:33 +0800

    创建逆向工程和前端的后端工程

---
+++ gmail-common/src/main/java/gmail/common/GmailCommonApplication.java
@@ -0,0 +1,13 @@
+package gmail.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmailCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmailCommonApplication.class, args);
+    }
+
+}
---
+++ gmail-common/src/main/java/gmail/common/to/SkuReductionTo.java
@@ -0,0 +1,18 @@
+package gmail.common.to;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.*;
+@Data
+public class SkuReductionTo {
+
+    private Long skuId;
+    private int fullCount;
+    private BigDecimal discount;
+    private int countStatus;
+    private  BigDecimal fullPrice;
+    private  BigDecimal reducePrice;
+    private  int priceStatus;
+    private  List<MemberPrice> memberPrice;
+}
